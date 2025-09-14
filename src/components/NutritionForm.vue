b8ca3679fa719c95ef09862cee42f94b7c13f292
Sarthak Jain <sarthak4556@gmail.com>
2025-09-14 16:12:16 +0530

    delete add

---
+++ src/components/NutritionForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <h2>{{ tagline }}</h2>
+  <form @submit.prevent="formsubmit" class="nutrition-form">
+    <label>Meals:</label>
+    <input type="text" placeholder="e.g. Breakfast" v-model="mealsnew"/>
+    <Label>Calories:</Label>
+    <input type="number" placeholder="Calories" v-model="calories"/>
+    <button>Submit</button>
+  </form>
+</template>
+
+<script>
+export default{
+  name:"NutritionForm",
+  props:['tagline'],
+  data()
+  {
+    return{
+      mealsnew:"",
+      calories:""
+    }
+  },
+  methods: 
+  {
+    formsubmit(){
+      this.$emit("addingmeals",{
+        meals:this.mealsnew,
+        calories:this.calories
+      }),
+    this.mealsnew="";
+    this.calories="";
+    },
+   
+  }
+}
+</script>
+
+<style>
+.nutrition-form{
+  display:flex;
+  flex-direction:row;
+  justify-content: center;
+  align-items: center;
+  gap:10px;
+  }
+</style>
--- src/components/NutritionPage.vue
+++ src/components/NutritionPage.vue
@@ -1,12 +1,56 @@
 <template>
-    <h1>This is Nutrition Page</h1>
+    <h1>This is the Nutrition Page</h1>
+    <NutritionForm tagline="Eat Healthy keeps your body fit" @addingmeals="addingdata"></NutritionForm>
+    <li v-for="(items,index) in nutritionList" :key="index" class="foodstyle">
+        {{index+1}} - {{ items.meals }} - {{ items.calories }}cal
+        <div class="buttons">
+            <button  class="edit-button">edit</button>
+            <button @click="deleting(index)">delete</button>    
+        </div>
+    </li>
+     
 </template>
-
 <script>
+import NutritionForm  from "./NutritionForm.vue";
 export default{
-    name:'NutritionPage'
+    name:"NutritionPage",
+    components:{
+        NutritionForm
+    },
+    data(){
+        return{
+            nutritionList:[]
+        }
+    },
+    methods:{
+        addingdata(newitem)
+        {
+            this.nutritionList.push(newitem);
+        },
+        deleting(index)
+        {
+            this.nutritionList.splice(index, 1);
+        },
+        
+    }
 }
 </script>
 
 <style>
+.foodstyle
+{
+    margin-top:30px;
+    background-color:#f1cef6;
+    width: 100px;
+    margin-left: 725px;
+}
+.buttons
+{
+    margin-top: 10px;
+    
+}
+.edit-button
+{
+    margin-right:10px;
+}
 </style>
